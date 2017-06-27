5a0326931f048031973e98853d037fecab32fbb6
imorelnunez <ivanmorel94@gmail.com>
2017-06-27 17:40:54 -0400

    change

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-
 import { AppComponent } from './app.component';
 import {HeroDetailComponent} from './hero-detail.component';
+import {HeroesComponent} from './hero.component';
+import {HeroListComponent} from './hero-list.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroDetailComponent
+    HeroDetailComponent,
+    HeroesComponent,
+    HeroListComponent
   ],
   imports: [
     BrowserModule,
     FormsModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent, HeroesComponent]
 })
 export class AppModule { }
---
+++ src/app/hero.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-heroes',
+  template: `
+    <h2>WTF HEROES</h2>
+    <hero-list></hero-list>
+  `
+})
+export class HeroesComponent { }
